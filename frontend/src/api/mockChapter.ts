f24709e9170e696feb84961ff00e7dedf8f311eb
Alicia Sutikno <99207714+sutiknoalicia@users.noreply.github.com>
2025-04-10 21:03:16 +0800

    Update mock data & MasteryChecklist (#30)

--- frontend/src/api/mockChapter.ts
+++ frontend/src/api/mockChapter.ts
@@ -1,6 +1,7 @@
 export interface ChapterData {
   title: string;
   mastery: string[];
+  mastery_status: boolean[];
   exercises: string[];
   progress: number;
   chats: {
@@ -16,7 +17,7 @@ export interface ChapterData {
 
 const chapterDatabase: Record<string, { id: string; title: string }[]> = {
   biology: [
-    { id: "cell-biology", title: "Cell Biology" },
+    { id: "life-processes", title: "Life Processes" },
     { id: "human-physiology", title: "Human Physiology" },
     { id: "ecology-environment", title: "Ecology & Environment" },
   ],
@@ -40,7 +41,44 @@ export const getChapterData = async (
 
   return {
     title: chapter,
-    mastery: ["Describe core concepts", "Explain real-world applications"],
+    mastery: [
+      "Understand the characteristics shared by living organisms",
+      "Describe cell structures and their functions, including the nucleus, cytoplasm, cell membrane, cell wall, mitochondria, chloroplasts, ribosomes and vacuole",
+      "Know the similarities and differences in the structures of plant and animal cells",
+      "Understand the role of enzymes as biological catalysts in metabolic reactions",
+      "Understand how temperature changes can affect enzyme function, including changes to the shape of the active site",
+      "Understand how enzyme function can be affected by changes in pH altering the active site",
+      "Investigate how enzyme activity can be affected by changes in temperature",
+      "Describe the differences between aerobic and anaerobic respiration",
+      "Understand how the process of respiration produces ATP in living organisms",
+      "Know that ATP provides energy for cells",
+      "Know the word equation and balanced chemical symbol equation for aerobic respiration",
+      "Know the word equations for anaerobic respiration",
+      "Investigate the evolution of carbon dioxide and heat from respiring seeds or other suitable living organisms",
+      "Understand the processes of diffusion, osmosis and active transport by which substances move into and out of cells",
+      "Understand how factors affect the rate of movement of substances into and out of cells",
+      "Investigate diffusion in a non-living system (agar jelly)",
+      "Describe the levels of organisation within organisms – organelles, cells, tissues, organ systems",
+    ],
+    mastery_status: [
+      true,
+      true,
+      true,
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+    ],
     exercises: ["A", "B", "C", "D", "E", "F", "G", "H"],
     progress: Math.floor(Math.random() * 100), // simulate varying progress
     chats: [
