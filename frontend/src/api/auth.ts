54d8f8a97b84ba54c66ecabc673e54fb49503bc8
sutiknoalicia <alicias@connect.hku.hk>
2025-04-13 23:15:32 +0800

    Auth file instead of login

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,3 +3,7 @@ import api from "@/helpers/axios";
 export const loginUser = (email: string, password: string) => {
   return api.post("/login", { email, password });
 };
+
+export const verifyUser = () => {
+  return api.post("/verify");
+};
