89ea0feeea5f8d25ca9f23610a0ce83c7b700c46
Bryan Melvison <bryanmel@connect.hku.hk>
2025-04-14 17:21:40 +0800

    Stash

---
+++ backend/app/utilities/auth.py
@@ -0,0 +1,3 @@
+# Kita pake ES256 krn its one of the most secure algorithms and also its the default algorithm for JWT
+# https://curity.io/resources/learn/jwt-best-practices/#:~:text=The%20option%20with%20the%20best,v1_5%20using%20SHA%2D256).
+
