4a1a7885e540850712e4375318c8f4d6be073116
sutiknoalicia <alicias@connect.hku.hk>
2025-04-14 23:00:42 +0800

    Adjust copywriting in ProgressBar

--- frontend/src/components/subjects/ProgressBar.tsx
+++ frontend/src/components/subjects/ProgressBar.tsx
@@ -1,7 +1,9 @@
 const ProgressBar = ({ progress }: { progress: number }) => {
   return (
     <div className="bg-white p-4 rounded-xl shadow-sm">
-      <h3 className="text-lg font-semibold text-gray-900 mb-2">Progress</h3>
+      <h3 className="text-lg font-semibold text-gray-900 mb-2">
+        Your Exercise Progress
+      </h3>
       <div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
         <div className="bg-sky-500 h-full" style={{ width: `${progress}%` }} />
       </div>
