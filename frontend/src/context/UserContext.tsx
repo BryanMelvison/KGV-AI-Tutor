6797b062ce32ac8dec603d9f6199d327abac0bb1
sutiknoalicia <alicias@connect.hku.hk>
2025-04-17 20:57:18 +0800

    sunset refresh-token

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -29,25 +29,27 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     const storedUser = localStorage.getItem("anh-user");
     const storedToken = localStorage.getItem("anh-token");
-    const storedRefreshToken = localStorage.getItem("anh-refresh-token");
+    // const storedRefreshToken = localStorage.getItem("anh-refresh-token");
 
-    if (storedUser && storedToken && storedRefreshToken) {
+    if (storedUser && storedToken) {
+      // if (storedUser && storedToken && storedRefreshToken) {
       setUser(JSON.parse(storedUser));
     }
     setUserLoaded(true);
   }, []);
 
-  const login = (user: User, access_token: string, refresh_token: string) => {
+  const login = (user: User, access_token: string) => {
+    // const login = (user: User, access_token: string, refresh_token: string) => {
     setUser(user);
     localStorage.setItem("anh-user", JSON.stringify(user));
-    localStorage.setItem("anh-refresh-token", refresh_token);
+    // localStorage.setItem("anh-refresh-token", refresh_token);
     localStorage.setItem("anh-token", access_token);
   };
 
   const logout = () => {
     setUser(null);
     localStorage.removeItem("anh-user");
-    localStorage.removeItem("anh-refresh-token");
+    // localStorage.removeItem("anh-refresh-token");
     localStorage.removeItem("anh-token");
   };
 
